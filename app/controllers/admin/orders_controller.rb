cfdd7a60cc1e3be6a148268af5e7c68b2e320397
Luis Henrique Witz <luiswitz@gmail.com>
2017-12-22 16:10:03 -0200

    Adjust status enum before save

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,5 +1,9 @@
 module Admin
   class OrdersController < Fae::BaseController
+    before_action :adjust_enum, only: [:create, :update]
 
+    def adjust_enum
+      params[:order][:status] = params[:order][:status].to_i
+    end
   end
 end
