d4f8cd8390099a13be20380899814e686b5721d9
Luis Henrique Witz <luiswitz@gmail.com>
2017-12-22 18:00:28 -0200

    Validations on supplier model

--- app/models/supplier.rb
+++ app/models/supplier.rb
@@ -1,6 +1,16 @@
 class Supplier < ApplicationRecord
   include Fae::BaseModelConcern
 
+  validates :name, presence: true
+  validates :phone, presence: true
+  validates :email,
+    uniqueness: true,
+    presence: true,
+    format: {
+      with: Fae.validation_helpers.email_regex,
+      message: 'A valid email format is required'
+    }
+
   def fae_display_field
     name
   end
