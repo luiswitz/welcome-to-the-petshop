0b081bd70d71390ac06673d79bbcfcb486cb1c77
Luis Henrique Witz <luiswitz@gmail.com>
2017-12-22 18:08:50 -0200

    Validations on service model

---
+++ spec/models/service_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe Service do
+  let(:service) do
+    Service.create(
+      title: 'the-title',
+      description: 'the-description',
+      price: 100,
+      observations: 'the-observations'
+    )
+  end
+
+  it 'is not valid without a title' do
+    service.title = nil
+    expect(service).to_not be_valid
+  end
+
+  it 'cant have a duplicated title' do
+    service2 = service.dup
+    expect(service2).to_not be_valid
+  end
+
+  it 'is not valid without a price' do
+    service.price = nil
+    expect(service).to_not be_valid
+  end
+
+end
