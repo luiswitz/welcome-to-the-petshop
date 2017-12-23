8d44359d05e2e3bd6d8bbfe1489b2f9c2a55efea
Luis Henrique Witz <luiswitz@gmail.com>
2017-12-23 10:16:29 -0200

    Associations on OrderProduct model

--- spec/models/order_product_spec.rb
+++ spec/models/order_product_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe OrderProduct, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'belongs to an order' do
+    association = described_class.reflect_on_association(:order)
+    expect(association.macro).to eq(:belongs_to)
+  end
+
+  it 'belongs to a product' do
+    association = described_class.reflect_on_association(:product)
+    expect(association.macro).to eq(:belongs_to)
+  end
 end
