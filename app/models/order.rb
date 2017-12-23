25f9652aa6e816ed195b0b334a273d6f92849d93
Luis Henrique Witz <luiswitz@gmail.com>
2017-12-23 10:10:06 -0200

    Associations on order model

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,6 +2,15 @@ class Order < ApplicationRecord
   include Fae::BaseModelConcern
   enum status: { finished: 0, canceled: 1 }
 
+  belongs_to :discount
+  belongs_to :client
+
+  has_many :order_products
+  has_many :products, through: :order_products
+
+  has_many :order_services
+  has_many :services, through: :order_services
+
   validates :client, presence: true
 
   def fae_display_field
@@ -12,6 +21,4 @@ class Order < ApplicationRecord
     order(:id)
   end
 
-  belongs_to :discount
-  belongs_to :client
 end
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -21,4 +21,24 @@ RSpec.describe Order do
     order.client = nil
     expect(order).to_not be_valid
   end
+
+  it 'belogns to a discount' do
+    association = described_class.reflect_on_association(:discount)
+    expect(association.macro).to eq(:belongs_to)
+  end
+
+  it 'belongs to a client' do
+    association = described_class.reflect_on_association(:client)
+    expect(association.macro).to eq(:belongs_to)
+  end
+
+  it 'has many products' do
+    association = described_class.reflect_on_association(:products)
+    expect(association.macro).to eq(:has_many)
+  end
+
+  it 'has many services' do
+    association = described_class.reflect_on_association(:services)
+    expect(association.macro).to eq(:has_many)
+  end
 end
