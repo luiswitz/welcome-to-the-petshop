4aeefee46473fb8a52d4942cacb7d36829f97a57
Luis Henrique Witz <luiswitz@gmail.com>
2017-12-23 10:14:39 -0200

    Associations on OrderService model

--- spec/models/order_service_spec.rb
+++ spec/models/order_service_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe OrderService, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'belongs to an order' do
+    association = described_class.reflect_on_association(:order)
+    expect(association.macro).to eq(:belongs_to)
+  end
+
+  it 'belongs to a service' do
+    association = described_class.reflect_on_association(:service)
+    expect(association.macro).to eq(:belongs_to)
+  end
 end
