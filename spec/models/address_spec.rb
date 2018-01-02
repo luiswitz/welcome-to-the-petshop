bd7e73369f440a83c4b1555b0e6823e108fd6507
Luis Henrique Witz <luiswitz@gmail.com>
2018-01-02 20:01:10 -0200

    Add validation to address model

--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Address, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it 'belongs to an adressable' do
+      association = described_class.reflect_on_association(:addressable)
+      expect(association.macro).to eq(:belongs_to)
+    end
+  end
+
+  describe 'validations' do
+    it 'is not valid without a name' do
+      address = described_class.new(name: '')
+      expect(address).to_not be_valid
+    end
+  end
 end
