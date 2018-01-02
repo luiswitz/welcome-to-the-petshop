58ab04e6cbbbd2d87d1a56793932cbdfd220792f
Luis Henrique Witz <luiswitz@gmail.com>
2018-01-02 19:54:01 -0200

    Run migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171231131118) do
+ActiveRecord::Schema.define(version: 20180102214901) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "addresses", force: :cascade do |t|
+    t.string   "name"
+    t.string   "addressable_type"
+    t.integer  "addressable_id"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+    t.index ["addressable_type", "addressable_id"], name: "index_addresses_on_addressable_type_and_addressable_id", using: :btree
+  end
 
   create_table "campaign_clients", force: :cascade do |t|
     t.integer  "campaign_id"
